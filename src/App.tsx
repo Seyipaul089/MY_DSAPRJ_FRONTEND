34c7be0d150320df1fa187c40d28ed52b8de4135
Seyipaul089 <olayemipaul089@gmail.com>
2025-07-16 14:21:54 +0100

    setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className='bg-black text-white'>
+        Welcome to our frontend project
+      </div>
+    </>
+  )
+}
+
+export default App
